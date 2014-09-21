421a581eeadf140a2862364d3c7b24cc01848def
David Reiman <david.reiman@gmail.com>
2014-09-20 19:33:18 -0700

    Refactor for clarity

--- deckofcards.js
+++ deckofcards.js
@@ -2,61 +2,75 @@
   var button = document.getElementsByTagName('button')[0],
       cardSpace = document.getElementsByClassName('card')[0];
 
+  // Card class
   function Card (rank, suit) {
     this.model = new Card.Model(rank, suit);
     this.view = new Card.View();
   };
 
+  Card.SUITS = ['spades', 'hearts', 'diamonds', 'clubs'];
+  Card.RANKS = ['ace', 'king', 'queen', 'jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two'];
+
   Card.prototype.renderTo = function (domElement) {
     this.view.renderTo(this.model, domElement);
   }
 
+  // Card.Model
   Card.Model = function (rank, suit) {
     this.rank = rank;
     this.suit = suit;
   };
 
+  // Card.View
   Card.View = function () {};
 
-  Card.View.prototype.generateUrlFrom = function (model) {
-    return 'img/' + model.rank + '-of-' + model.suit + '.png';
-  }
+  Card.View.prototype = {
+    generateUrlFrom: function (model) {
+      return 'img/' + model.rank + '-of-' + model.suit + '.png';
+    },
 
-  Card.View.prototype.renderTo = function (model, domElement) {
-    domElement.style.backgroundImage = 'url("' + this.generateUrlFrom(model) + '")';
-  }
+    renderTo: function (model, domElement) {
+      domElement.style.backgroundImage = 'url("' + this.generateUrlFrom(model) + '")';
+    }
+  };
 
-  Card.SUITS = ['spades', 'hearts', 'diamonds', 'clubs'];
-  Card.RANKS = ['ace', 'king', 'queen', 'jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two'];
-
+  // Deck
   function Deck () {
     this.model = new Deck.Model();
-  }
-
-  Deck.Model = function () {
-    this.cards = this.generateAFullDeck();
-    this.shuffle();
-  };
-
-  Deck.Model.prototype.generateAFullDeck = function () {
-    var allCards = [];
-    for (var i = 0; i < Card.SUITS.length; i++)
-      for (var j = 0; j < Card.RANKS.length; j++)
-        allCards.push( new Card( Card.RANKS[j], Card.SUITS[i] ) );
-    return allCards;
-  };
-
-  Deck.Model.prototype.shuffle = function () {
-    this.cards.sort(function () { return 2 * Math.round(Math.random()) - 1 });
   };
 
   Deck.prototype.drawCard = function () {
     return this.model.drawCard();
   };
 
-  Deck.Model.prototype.drawCard = function () {
-    return this.cards.pop();
-  }
+  // Deck.Model
+  Deck.Model = function () {
+    this.cards = this.generateAFullDeck();
+    this.shuffle();
+  };
+
+  Deck.Model.prototype = {
+    drawCard = function () {
+      return this.cards.pop();
+    },
+
+    generateAFullDeck: function () {
+      var allCards = [];
+      for (var i = 0; i < Card.SUITS.length; i++)
+        for (var j = 0; j < Card.RANKS.length; j++)
+          allCards.push( new Card( Card.RANKS[j], Card.SUITS[i] ) );
+      return allCards;
+    },
+
+    shuffle: function () {
+      // sort takes a function that compares two elements; the function should
+      // return 1 if the first element should be to the right of the second element
+      // return 0 if the two elements are equivalent
+      // return -1 if the first element should be to the left of the second element
+      // to 'shuffle', we generate a function that randomly returns either 1 or -1
+      this.cards.sort(function () { return 2 * Math.round(Math.random()) - 1 });
+    }
+  };
 
   var deck = new Deck();
 
