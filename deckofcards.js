532b9a841ac165ea23c0950b25447efa4290dcb1
David Reiman <david.reiman@gmail.com>
2014-09-20 19:24:53 -0700

    Use deck object instead of plain array

--- deckofcards.js
+++ deckofcards.js
@@ -1,6 +1,5 @@
 ;(function () {
-  var allCards = [],
-      button = document.getElementsByTagName('button')[0],
+  var button = document.getElementsByTagName('button')[0],
       cardSpace = document.getElementsByClassName('card')[0];
 
   function Card (rank, suit) {
@@ -30,14 +29,38 @@
   Card.SUITS = ['spades', 'hearts', 'diamonds', 'clubs'];
   Card.RANKS = ['ace', 'king', 'queen', 'jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two'];
 
-  for (var i = 0; i < Card.SUITS.length; i++) {
-    for (var j = 0; j < Card.RANKS.length; j++) {
-      allCards.push( new Card( Card.RANKS[j], Card.SUITS[i] ) );
-    }
+  function Deck () {
+    this.model = new Deck.Model();
   }
 
+  Deck.Model = function () {
+    this.cards = this.generateAFullDeck();
+    this.shuffle();
+  };
+
+  Deck.Model.prototype.generateAFullDeck = function () {
+    var allCards = [];
+    for (var i = 0; i < Card.SUITS.length; i++)
+      for (var j = 0; j < Card.RANKS.length; j++)
+        allCards.push( new Card( Card.RANKS[j], Card.SUITS[i] ) );
+    return allCards;
+  };
+
+  Deck.Model.prototype.shuffle = function () {
+    this.cards.sort(function () { return 2 * Math.round(Math.random()) - 1 });
+  };
+
+  Deck.prototype.drawCard = function () {
+    return this.model.drawCard();
+  };
+
+  Deck.Model.prototype.drawCard = function () {
+    return this.cards.pop();
+  }
+
+  var deck = new Deck();
+
   button.addEventListener('click', function () {
-    var cardToDeal = allCards.pop();
-    cardToDeal.renderTo(cardSpace);
+    deck.drawCard().renderTo(cardSpace);
   });
 })();
